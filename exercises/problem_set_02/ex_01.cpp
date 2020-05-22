e1915de24417c1a643f0f25626d77ce7762ad3ff
R. Nicolás Savinelli <rsavinelli@est.frba.utn.edu.ar>
2020-05-22 12:22:00 -0300

    equivalent logical statement

--- exercises/problem_set_02/ex_01.cpp
+++ exercises/problem_set_02/ex_01.cpp
@@ -32,7 +32,7 @@ using namespace std;
 
 bool is_leap(int year)
 {
-	if (((year % 4) == 0 && (year % 100) != 0) || (year % 400) == 0) {
+	if ((year % 4) == 0 && (((year % 100) != 0) || (year % 400) == 0)) {
 		return true;
 	}
 

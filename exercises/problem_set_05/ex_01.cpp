207ac07f03c9aef4bf262d038358af79dadf4b51
R. Nicolás Savinelli <rsavinelli@est.frba.utn.edu.ar>
2020-09-26 18:47:08 -0300

    add task

--- exercises/problem_set_05/ex_01.cpp
+++ exercises/problem_set_05/ex_01.cpp
@@ -1,6 +1,11 @@
 /*
  * ex_01.cpp
  *
+ * Dada una pila programe una función dup (por duplicar) que reciba la pila y
+ * duplique el elemento que está en el tope (a menos que la pila esté vacía).
+ * Por ejemplo si la pila un 1 al tope seguido más abajo de un 2 (Notación: 1,
+ * 2) luego de invocar esta función debe tener 1, 1, 2.
+ *
  * Copyright 2020 Roberto Nicolás Savinelli <rnsavinelli@est.frba.utn.edu.ar>
  *
  * This program is free software; you can redistribute it and/or modify

474cb276d71bd2f85ef2ccc26e4bbd88fe42e097
R. Nicolás Savinelli <rsavinelli@est.frba.utn.edu.ar>
2020-11-19 20:03:20 -0300

    functional headerfiles

--- exercises/problem_set_07/linkedlists/linkedLists.h
+++ exercises/problem_set_07/linkedlists/linkedLists.h
@@ -6,18 +6,18 @@ template <typename T> struct Node {
 	Node<T>* next = nullptr;
 };
 
-template <typename T> void push(Node<T>* &stack, T value);
+template <typename T> void push(Node<T>* &stack, T key);
+template <typename T> void append(Node<T>*& stack, T key);
 template <typename T> T pop(Node<T>* &stack);
 template <typename T> void stackErase(Node<T>* &stack);
 
-template <typename T> int stackEmpty(const Node<T>* stack);
-template <typename T> void append(Node<T>*& stack, T value);
+template <typename T> bool stackEmpty(const Node<T>* stack);
 
 template <typename T> void stackPrint(Node<T>* stack);
 
 template <typename T> bool linear_search(T key, Node<T>* statck, int (*criteria)(T, T));
 template <typename T> Node<T>* search(T key, Node<T>* stack, int (*criteria)(T, T));
-template <typename T> void insert(T value, Node<T>* &stack, int (*criteria)(T, T));
+template <typename T> void insert(T key, Node<T>* &stack, int (*criteria)(T, T));
 template <typename T> Node<T>* insert_unique(T key, Node<T>* &stack, int (*criteria)(T, T));
 
 template <typename T> Node<T>* extract(T key, Node<T>* &stack, int (*criteria)(T, T));
